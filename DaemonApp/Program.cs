2389027bb0284660a7310e7453aeac8a2ea9a3b5
mmusaev <9410421+mmusaev@users.noreply.github.com>
2024-06-14 16:38:31 -0500

    update to use msal instead of adal

--- DaemonApp/Program.cs
+++ DaemonApp/Program.cs
@@ -1,16 +1,15 @@
-﻿using Azure.Identity;
+﻿using Azure;
+using Azure.Identity;
 using Azure.Security.KeyVault.Certificates;
 using Azure.Security.KeyVault.Secrets;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Identity.Client;
 using System;
 using System.Configuration;
 using System.Globalization;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading;
 using System.Threading.Tasks;
-using Azure;
 
 namespace DaemonApp
 {
@@ -25,8 +24,6 @@ namespace DaemonApp
         static string authority = string.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
 
         private static HttpClient httpClient = new HttpClient();
-        private static AuthenticationContext authContext = new AuthenticationContext(authority);
-        private static ClientAssertionCertificate certCred = null;
 
         private static string audienceUri = ConfigurationManager.AppSettings["ida:AudienceUri"];
 
@@ -49,7 +46,7 @@ namespace DaemonApp
 
         static async Task<string> CallAPI()
         {
-            AuthenticationResult result = await GetAccessToken(audienceUri);
+            AuthenticationResult result = await GetAccessTokenWithMSAL(audienceUri);
             if (result == null)
             {
                 Console.WriteLine("Canceling attempt to call graph.\n");
@@ -59,60 +56,83 @@ namespace DaemonApp
             {
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
                 HttpResponseMessage response = await httpClient.GetAsync(graphUrl);
-                if (response.IsSuccessStatusCode == true)
+                if (response.IsSuccessStatusCode)
                 {
                     return await response.Content.ReadAsStringAsync();
                 }
                 else
                 {
-                    Console.WriteLine("Failed to call graph.\nError:  {0}\n", response.ReasonPhrase);
+                    Console.WriteLine($"Failed to call graph.\nError:  {response.ReasonPhrase}\n");
                     return null;
                 }
             }
         }
 
-        private static async Task<AuthenticationResult> GetAccessToken(string resourceId)
-        {
-            AuthenticationResult result = null;
-            int retryCount = 0;
-            bool retry = false;
 
-            // Initialize the Certificate Credential to be used by ADAL.
+        private static async Task<AuthenticationResult> GetAccessTokenWithMSAL(string resourceId)
+        {
             X509Certificate2 cert = await GetCertificateFromKeyVault(certName);
             if (cert == null)
             {
                 Console.WriteLine($"Cannot find active certificate '{certName}' in Azure Key Vault. Please check configuration");
+                return null;
             }
 
-            certCred = new ClientAssertionCertificate(clientId, cert);
+            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(clientId)
+                .WithAuthority(new Uri(authority))
+                .WithCertificate(cert)
+                .Build();
 
-            do
-            {
-                retry = false;
+            string[] scopes = new string[] { $"{resourceId}/.default" };
 
-                try
-                {
-                    result = await authContext.AcquireTokenAsync(resourceId, certCred);
-                }
-                catch (AdalException ex)
-                {
-                    if (ex.ErrorCode == "temporarily_unavailable")
-                    {
-                        retry = true;
-                        retryCount++;
-                        Thread.Sleep(3000);
-                    }
+            AuthenticationResult result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
 
-                    Console.WriteLine(
-                        String.Format("An error occurred while acquiring a token\nTime: {0}\nError: {1}\nRetry: {2}\n",
-                        DateTime.Now.ToString(),
-                        ex.ToString(),
-                        retry.ToString()));
-                }
-
-            } while ((retry == true) && (retryCount < 3));
             return result;
         }
+
+
+        //private static async Task<AuthenticationResult> GetAccessToken(string resourceId)
+        //{
+        //    AuthenticationResult result = null;
+        //    int retryCount = 0;
+        //    bool retry = false;
+
+        //    // Initialize the Certificate Credential to be used by ADAL.
+        //    X509Certificate2 cert = await GetCertificateFromKeyVault(certName);
+        //    if (cert == null)
+        //    {
+        //        Console.WriteLine($"Cannot find active certificate '{certName}' in Azure Key Vault. Please check configuration");
+        //    }
+
+        //    certCred = new ClientAssertionCertificate(clientId, cert);
+
+        //    do
+        //    {
+        //        retry = false;
+
+        //        try
+        //        {
+        //            result = await authContext.AcquireTokenAsync(resourceId, certCred);
+        //        }
+        //        catch (AdalException ex)
+        //        {
+        //            if (ex.ErrorCode == "temporarily_unavailable")
+        //            {
+        //                retry = true;
+        //                retryCount++;
+        //                Thread.Sleep(3000);
+        //            }
+
+        //            Console.WriteLine(
+        //                String.Format("An error occurred while acquiring a token\nTime: {0}\nError: {1}\nRetry: {2}\n",
+        //                DateTime.Now.ToString(),
+        //                ex.ToString(),
+        //                retry.ToString()));
+        //        }
+
+        //    } while ((retry == true) && (retryCount < 3));
+        //    return result;
+        //}
 
         private static async Task<X509Certificate2> GetCertificateFromKeyVault(string certName)
         {
